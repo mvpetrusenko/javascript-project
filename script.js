4b5b15ba009140633bf684d2c8e8c03423c5e19d
mvpetrusenko <mvpetrusenko300@gmail.com>
2024-08-09 20:49:34 +0300

    initial commit

---
+++ script.js
@@ -0,0 +1,3 @@
+document.getElementById('agreed').addEventListener('click', function() {
+    document.getElementById('output').innerHTML = 'Your data will be processing according to your agreement';
+})
