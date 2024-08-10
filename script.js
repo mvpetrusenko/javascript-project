be3852311e634f1f31b48cdffeaade8a34823214
mvpetrusenko <mvpetrusenko300@gmail.com>
2024-08-10 16:31:48 +0300

    added contacts file

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
-document.getElementById('agreed').addEventListener('click', function() {
-    document.getElementById('output').innerHTML = 'Your data will be processing according to your agreement';
+// document.getElementById('agreed').addEventListener('click', function() {
+//     document.getElementById('output').innerHTML = 'Your data will be processing according to your agreement';
+// })v
+
+
+document.getElementById('send').addEventListener('click', function() {
+    document.getElementById('send').style.backgroundColor = 'red';
 })
